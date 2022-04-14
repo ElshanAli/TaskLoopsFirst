e14c1dfc229798512cdc1d5c0c30134c60d8435c
ElshanAli <elshanml@code.edu.az>
2022-04-14 18:13:56 +0400

    Added firsrt task

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TaskLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter words ");
+            
+            
+            string[] wordsArr = new string[] { Console.ReadLine() };
+            
+
+            for (int i = 0; i < wordsArr.Length; i++)
+            {
+                if (wordsArr[i].Length > 5)
+                {
+                    Console.WriteLine(wordsArr[i] + " " + "Length is greater than 5");
+                }
+                else
+                {
+                    Console.WriteLine(wordsArr[i] + " " + "Length is less than 5");
+                }
+            }
+          
+        }
+    }
+}
